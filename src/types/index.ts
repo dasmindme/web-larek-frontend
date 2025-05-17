c8e1de88cb2f6857c6648277507e1dcb9042154e
unknown <vlsova.irina.ru@yandex.ru>
2025-05-17 10:51:04 +0300

    1 commit

---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+
+
+export interface IShopItem {
+    id: string;
+    image: string;
+    category: string;
+    title: string;
+    about: string;
+    price: number;
+}
+
+export type IBasketItem = Pick<IShopItem, 'id' | 'title' | 'price'>;
+
+export interface IAppState {
+    catalog: IShopItem[];
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+    loading: boolean;
+}
+
+export interface IOrderForm {
+    email: string;
+    phone: string;
+}
+
+export interface IOrder extends IOrderForm {
+    items: string[]
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IBid {
+    price: number
+}
+
+export interface IOrderResult {
+    id: string;
+}
